d27dba84baad3415eae44faf362f17c833478b3e
Matej Lubej <matej.lubej@oasisprotocol.org>
2025-05-04 17:36:33 +0200

    Add shadcn/aspect-ratio

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export * from '@/components/ui/accordion'
 export * from '@/components/ui/alert'
 export * from '@/components/ui/alert-dialog'
+export * from '@/components/ui/aspect-ratio'
 export * from '@/components/ui/button'
