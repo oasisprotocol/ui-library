7ddaa4b0311c7346f59df1f2f26d1f98c0d05e7d
Michal Zielenkiewicz <michal@oasisprotocol.org>
2025-05-08 11:26:28 +0200

    Add Alert component stories

---
+++ src/stories/Alert/Alert.stories.tsx
@@ -0,0 +1,40 @@
+import type { Meta, StoryObj } from '@storybook/react-vite'
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert.tsx'
+import { InfoIcon } from 'lucide-react'
+import { expect, within } from 'storybook/test'
+
+const meta: Meta<typeof Alert> = {
+  title: 'Components/Alert',
+  component: Alert,
+  parameters: {
+    layout: 'centered',
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/design/dSsI9L6NSpNCorbSdiYd1k/Oasis-Design-System---shadcn-ui---Default---December-2024?node-id=21-322&p=f&t=RSGCFmRgOgVUlGFP-0',
+    },
+  },
+  tags: ['autodocs'],
+}
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  args: {
+    children: (
+      <>
+        <InfoIcon />
+        <AlertTitle>Information</AlertTitle>
+        <AlertDescription>
+          Alert description provides additional information about the alert.
+        </AlertDescription>
+      </>
+    ),
+    variant: 'default',
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement)
+    const alert = canvas.getByRole('alert')
+    await expect(alert).toBeInTheDocument()
+  },
+}
