ca20dfe364640f61b345f8e36b1793ae44c44174
Kristof Csillag <kristof@oasisprotocol.org>
2025-06-17 19:09:06 +0200

    Add more terse format for defining boolean fields

--- src/components/ui-plus-behavior/input/useBoolField.ts
+++ src/components/ui-plus-behavior/input/useBoolField.ts
@@ -1,4 +1,5 @@
 import { InputFieldControls, InputFieldProps, useInputField } from './useInputField.ts'
+import { MarkdownCode } from '../../ui/markdown.tsx'
 
 type BoolFieldProps = Omit<InputFieldProps<boolean>, 'initialValue' | 'placeholder' | 'compact'> & {
   initialValue?: boolean
@@ -7,7 +8,15 @@ type BoolFieldProps = Omit<InputFieldProps<boolean>, 'initialValue' | 'placehold
 
 export type BooleanFieldControls = InputFieldControls<boolean> & Pick<BoolFieldProps, 'preferredWidget'> & {}
 
-export function useBooleanField(props: BoolFieldProps): BooleanFieldControls {
+export function useBooleanField(name: string, description?: MarkdownCode): BooleanFieldControls
+
+export function useBooleanField(props: BoolFieldProps): BooleanFieldControls
+
+export function useBooleanField(
+  rawProps: BoolFieldProps | string,
+  description?: MarkdownCode
+): BooleanFieldControls {
+  const props = typeof rawProps === 'string' ? { name: rawProps, description } : rawProps
   const { initialValue = false, preferredWidget = 'checkbox' } = props
 
   const controls = useInputField<boolean>(
