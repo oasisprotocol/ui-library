9812117d2d95d112b1c1f4bb2af67cfab17fc045
Kristof Csillag <kristof@oasisprotocol.org>
2025-06-19 15:58:37 +0200

    Clean up storybook for ActionButton

--- src/stories/FormBuilding/ActionButton.stories.tsx
+++ src/stories/FormBuilding/ActionButton.stories.tsx
@@ -1,11 +1,26 @@
 import type { Meta, StoryObj } from '@storybook/react-vite'
-import { expect, within } from 'storybook/test'
-import { ActionButton, deny, sleep, useAction } from '../../components/ui-plus-behavior/input'
-import { useState } from 'react'
+import { expect, fn, userEvent, within } from 'storybook/test'
+import {
+  ActionButton,
+  ActionProps,
+  camelToTitleCase,
+  deny,
+  FullConfirmationRequest,
+  sleep,
+  useAction,
+} from '../../components/ui-plus-behavior/input'
+import { FC } from 'react'
 
-const meta: Meta<typeof ActionButton> = {
+import { screen } from 'storybook/test'
+
+const ActionButtonTest: FC<ActionProps> = props => <ActionButton {...useAction(props)} />
+
+const meta: Meta<typeof ActionButtonTest> = {
   title: 'ui-plus-behavior/useAction() and <ActionButton>',
-  component: ActionButton,
+  component: ActionButtonTest,
+  args: {
+    action: fn(),
+  },
   parameters: {
     docs: {
       description: {
@@ -21,318 +36,335 @@ export default meta
 type Story = StoryObj<typeof meta>
 
 export const Default: Story = {
-  render: function DefaultExample() {
-    const [counter, setCounter] = useState(0)
-    const action = useAction({
-      name: 'test action',
-      action: () => setCounter(c => c + 1),
-    })
-
-    return (
-      <div className={'w-[400px]'}>
-        <ActionButton {...action} /> {/* This is our component */}
-        <p>Action executed {counter} times.</p>
-      </div>
-    )
+  args: {
+    name: 'testAction',
   },
-  play: async ({ canvasElement }) => {
+
+  play: async ({ args, canvasElement }) => {
     const canvas = within(canvasElement)
-    const button = canvas.getAllByRole('button', { name: 'Test button' })[0]
+    const button = canvas.getAllByRole('button', { name: 'Test action' })[0]
     await expect(button).toBeInTheDocument()
+    await userEvent.click(button)
+    await expect(args.action).toBeCalled()
   },
 }
 
-export const Customized: Story = {
-  render: function Example() {
-    const [counter, setCounter] = useState(0)
-
-    const action = useAction({
-      name: 'customizedAction',
-      label: 'Customized **Action Button**',
-      variant: 'destructive',
-      size: 'lg',
-      action: () => setCounter(c => c + 1),
-    })
-
-    return (
-      <div className={'w-[400px]'}>
-        <ActionButton {...action} /> {/* This is our component */}
-        <p>Action executed {counter} times.</p>
-      </div>
-    )
+export const CustomizedButton: Story = {
+  args: {
+    name: 'customizedAction',
+    label: 'Customized **action button**',
+    variant: 'destructive',
+    size: 'lg',
   },
+
   play: async ({ canvasElement }) => {
     const canvas = within(canvasElement)
-    const button = canvas.getAllByRole('button', { name: 'Test button' })[0]
-    await expect(button).toBeInTheDocument()
+    const button = canvas.getAllByRole('button', { name: 'Customized action button' })[0]
+    await expect(button).toContainHTML('<strong>action button</strong>')
   },
 }
 
 export const WithDescription: Story = {
-  render: function Example() {
-    const [counter, setCounter] = useState(0)
-    const action = useAction({
-      name: 'descriptionAction',
-      label: 'Action with description',
-      description: 'There is a _description_ to this',
-      action: () => setCounter(c => c + 1),
-    })
-
-    return (
-      <div className={'w-[400px]'}>
-        <ActionButton {...action} /> {/* This is our component */}
-        <p>Action executed {counter} times.</p>
-      </div>
-    )
+  args: {
+    name: 'actionWithDescription',
+    description: 'There is a _description_ to this',
   },
-  play: async ({ canvasElement }) => {
-    const canvas = within(canvasElement)
-    const button = canvas.getAllByRole('button', { name: 'Test button' })[0]
-    await expect(button).toBeInTheDocument()
-  },
-}
-
-export const WithBooleanConfirmation: Story = {
-  render: function Example() {
-    const [counter, setCounter] = useState(0)
-    const action = useAction({
-      name: 'confirmedAction',
-      label: 'Do something **wild**',
-      confirmationNeeded: true,
-      action: () => setCounter(c => c + 1),
-    })
-
-    return (
-      <div className={'w-[400px]'}>
-        <ActionButton {...action} /> {/* This is our component */}
-        <p>Action executed {counter} times.</p>
-      </div>
-    )
-  },
-  play: async ({ canvasElement }) => {
-    const canvas = within(canvasElement)
-    const button = canvas.getAllByRole('button', { name: 'Test button' })[0]
-    await expect(button).toBeInTheDocument()
-  },
-}
-
-export const WithStringConfirmation: Story = {
-  render: function Example() {
-    const [counter, setCounter] = useState(0)
-    const action = useAction({
-      name: 'confirmedAction',
-      label: 'Do something **wild**',
-      confirmationNeeded: 'Have you considered the consequences?',
-      action: () => setCounter(c => c + 1),
-    })
-
-    return (
-      <div className={'w-[400px]'}>
-        <ActionButton {...action} /> {/* This is our component */}
-        <p>Action executed {counter} times.</p>
-      </div>
-    )
-  },
-  play: async ({ canvasElement }) => {
-    const canvas = within(canvasElement)
-    const button = canvas.getAllByRole('button', { name: 'Test button' })[0]
-    await expect(button).toBeInTheDocument()
-  },
-}
-
-export const WithCustomizedConfirmation: Story = {
-  render: function Example() {
-    const [counter, setCounter] = useState(0)
-    const action = useAction({
-      name: 'confirmedAction',
-      label: 'Do something **wild**',
-      confirmationNeeded: {
-        description: 'Do you **really** want to do this?',
-        cancelLabel: 'I would rather not',
-        okLabel: 'LFG',
-        variant: 'destructive',
+  parameters: {
+    docs: {
+      description: {
+        story: 'Try hovering the pointer',
       },
-      action: () => setCounter(c => c + 1),
-    })
-
-    return (
-      <div className={'w-[400px]'}>
-        <ActionButton {...action} /> {/* This is our component */}
-        <p>Action executed {counter} times.</p>
-      </div>
-    )
+    },
   },
   play: async ({ canvasElement }) => {
     const canvas = within(canvasElement)
-    const button = canvas.getAllByRole('button', { name: 'Test button' })[0]
+    const button = canvas.getAllByRole('button', { name: 'Action with description' })[0]
     await expect(button).toBeInTheDocument()
+
+    // Should show an info icon
+    const infoIcon = canvasElement.querySelector('.lucide-info')
+    await expect(infoIcon).toBeInTheDocument()
+
+    // Hovering should bring up the tooltip, with the formatted markdown
+    await userEvent.hover(button, { delay: 500 })
+    const tooltip = screen.getByRole('tooltip')
+    await expect(tooltip).toContainHTML('There is a <em>description</em> to this')
   },
 }
+export const WithConfirmation: Story = {
+  args: {
+    name: 'doSomethingWild',
+    confirmationNeeded: true,
+  },
+
+  play: async ({ args, canvasElement }) => {
+    const canvas = within(canvasElement)
+    const button = canvas.getAllByRole('button', { name: 'Do something wild' })[0]
+    await expect(button).toBeInTheDocument()
+
+    // When clicked, dialog is shown
+    await userEvent.click(button, { delay: 500 })
+
+    let dialog = screen.getByRole('dialog')
+    await expect(dialog).toBeVisible()
+
+    // We can cancel it, dialog disappears, no action
+    const cancelButton = screen.getByTestId('cancel')
+    await expect(cancelButton).toBeInTheDocument()
+    await userEvent.click(cancelButton, { delay: 500 })
+    await expect(dialog).not.toBeVisible()
+    await expect(args.action).not.toBeCalled()
+
+    // When clicked again, dialog is shown again
+    await userEvent.click(button, { delay: 500 })
+    dialog = screen.getByRole('dialog')
+    await expect(dialog).toBeVisible()
+
+    // We can confirm it, dialog disappears, action is executed
+    const continueButton = screen.getByTestId('confirm')
+    await expect(continueButton).toBeInTheDocument()
+    await userEvent.click(continueButton, { delay: 500 })
+    await expect(dialog).not.toBeVisible()
+    await expect(args.action).toBeCalled()
+  },
+}
+
+export const WithCustomizedConfirmationQuestion: Story = {
+  args: {
+    name: 'doSomethingWild',
+    confirmationNeeded: 'Have you considered the consequences?',
+  },
+
+  play: async ({ args, canvasElement }) => {
+    const canvas = within(canvasElement)
+    const button = canvas.getAllByRole('button', { name: 'Do something wild' })[0]
+    await expect(button).toBeInTheDocument()
+
+    // When clicked, dialog is shown
+    await userEvent.click(button, { delay: 500 })
+
+    let dialog = screen.getByRole('dialog')
+    await expect(dialog).toBeVisible()
+
+    await expect(dialog).toContainHTML(args.confirmationNeeded as string)
+  },
+}
+
+export const WithCustomizedConfirmationDialog: Story = {
+  args: {
+    name: 'doSomethingWild',
+    confirmationNeeded: {
+      title: 'Attention',
+      description: 'Do you **really** want to do this?',
+      cancelLabel: 'I would rather not',
+      okLabel: 'LFG',
+      variant: 'destructive',
+    },
+  },
+
+  play: async ({ args, canvasElement }) => {
+    const request = args.confirmationNeeded as FullConfirmationRequest
+    const canvas = within(canvasElement)
+
+    const button = canvas.getAllByRole('button', { name: 'Do something wild' })[0]
+    await expect(button).toBeInTheDocument()
+
+    // When clicked, dialog is shown
+    await userEvent.click(button, { delay: 500 })
+    let dialog = screen.getByRole('dialog')
+    await expect(dialog).toBeVisible()
+
+    // The title, description and button labels are customized
+    const title = document.querySelector('[data-slot="dialog-title"]')
+    await expect(title).toHaveTextContent(request.title.toString())
+
+    const description = document.querySelector('[data-slot="dialog-description"]')
+    // Note the rendered Markdown formatting
+    await expect(description).toContainHTML('Do you <strong>really</strong> want to do this?')
+
+    const cancelButton = screen.getByTestId('cancel')
+    await expect(cancelButton).toHaveTextContent(request.cancelLabel.toString())
+
+    const confirmButton = screen.getByTestId('confirm')
+    await expect(confirmButton).toHaveTextContent(request.okLabel.toString())
+  },
+}
+
+const EXCUSE = "I'm sorry Dave, I'm afraid I can't do that."
 
 export const Disabled: Story = {
-  render: function Example() {
-    const [counter, setCounter] = useState(0)
-    const action = useAction({
-      name: 'disabledAction',
-      label: 'Open the bay doors',
-      enabled: deny("I'm sorry Dave, I'm afraid I can't do that."),
-      action: () => setCounter(c => c + 1),
-    })
-
-    return (
-      <div className={'w-[400px]'}>
-        <ActionButton {...action} /> {/* This is our component */}
-        <p>Action executed {counter} times.</p>
-      </div>
-    )
+  args: {
+    name: 'disabledAction',
+    label: 'Open the bay doors',
+    enabled: deny(EXCUSE),
   },
-  play: async ({ canvasElement }) => {
+  play: async ({ args, canvasElement }) => {
     const canvas = within(canvasElement)
-    const button = canvas.getAllByRole('button', { name: 'Test button' })[0]
+
+    // We get a button
+    const button = canvas.getAllByRole('button', { name: args.label!.toString() })[0]
     await expect(button).toBeInTheDocument()
+
+    // Hovering should bring up the tooltip,
+    // which should have the description, formatted as MarkDown
+    await userEvent.hover(button, { delay: 500 })
+    const tooltip = screen.getByRole('tooltip')
+    await expect(tooltip).toHaveTextContent(EXCUSE)
+
+    // Clicking does nothing
+    await userEvent.click(button)
+    await expect(args.action).not.toBeCalled()
   },
 }
 
 export const LongAction: Story = {
-  render: function Example() {
-    const [counter, setCounter] = useState(0)
-    const action = useAction({
-      name: 'longAction',
-      label: 'Calculate the answer',
-      pendingLabel: 'Calculating',
-      action: async () => {
-        await sleep(2000)
-        setCounter(c => c + 1)
-      },
-    })
-
-    return (
-      <div className={'w-[400px]'}>
-        <ActionButton {...action} /> {/* This is our component */}
-        <p>Action executed {counter} times.</p>
-      </div>
-    )
+  args: {
+    name: 'calculateTheAnswer',
+    pendingLabel: 'Calculating',
+    action: fn(async () => await sleep(1000)),
   },
-  play: async ({ canvasElement }) => {
+  play: async ({ args, canvasElement }) => {
+    // We get a button
     const canvas = within(canvasElement)
-    const button = canvas.getAllByRole('button', { name: 'Test button' })[0]
+    const button = canvas.getByRole('button')
     await expect(button).toBeInTheDocument()
+
+    // Clicking will execute the action
+    await userEvent.click(button)
+
+    // Pending state will be indicated with spinner
+    let spinner = canvasElement.querySelector('.lucide-loader-circle')
+    await expect(spinner).toBeInTheDocument()
+
+    // label will be replaced with pending label
+    await expect(button).toHaveTextContent(args.pendingLabel!.toString())
+
+    // Wait for the action to finish
+    await sleep(1100)
+
+    // label will be restored to original content
+    await expect(button).toHaveTextContent(camelToTitleCase(args.name))
+
+    // Spinner will be gone
+    spinner = canvasElement.querySelector('.lucide-loader-circle')
+    await expect(spinner).toBeNull()
   },
 }
 
-export const ActionWithExecutionStatus: Story = {
-  render: function Example() {
-    const [counter, setCounter] = useState(0)
-    const action = useAction({
-      name: 'longAction',
-      label: 'Calculate the answer',
-      pendingLabel: 'Calculating',
-      action: async context => {
-        context.setStatus('Phase _one_..')
-        await sleep(2000)
-        context.setStatus('Phase **two**..')
-        await sleep(2000)
-        setCounter(c => c + 1)
-      },
-    })
-
-    return (
-      <div className={'w-[400px]'}>
-        <ActionButton {...action} /> {/* This is our component */}
-        <p>Action executed {counter} times.</p>
-      </div>
-    )
+export const WithExecutionStatus: Story = {
+  args: {
+    name: 'calculateTheAnswer',
+    action: fn(async ({ setStatus }) => {
+      setStatus('Phase one..')
+      await sleep(500)
+      setStatus('Phase two..')
+      await sleep(500)
+    }),
   },
+
   play: async ({ canvasElement }) => {
+    // We get a button
     const canvas = within(canvasElement)
-    const button = canvas.getAllByRole('button', { name: 'Test button' })[0]
+    const button = canvas.getByRole('button')
     await expect(button).toBeInTheDocument()
+
+    // Clicking will start the process, and first phase message is shown
+    await userEvent.click(button)
+    const status = canvas.getByRole('status-message')
+    await expect(status).toHaveTextContent('Phase one')
+
+    // Wait for phase 1 to finish
+    await sleep(550)
+
+    // Second phase message is visible
+    await expect(status).toHaveTextContent('Phase two')
+
+    // Wait for phase 2 to finish
+    await sleep(550)
+
+    // Status message is no longer shown
+    await expect(status).not.toBeInTheDocument()
   },
 }
+
+const WARNING = 'I have a bad feeling about this.'
 
 export const WithWarning: Story = {
-  render: function Example() {
-    const [counter, setCounter] = useState(0)
-    const action = useAction({
-      name: 'longAction',
-      label: 'Just do it',
-      pendingLabel: 'Doing it',
-      action: async context => {
-        await sleep(2000)
-        context.warn('I have a bad feeling about this.')
-        setCounter(c => c + 1)
-      },
-    })
-
-    return (
-      <div className={'w-[400px]'}>
-        <ActionButton {...action} /> {/* This is our component */}
-        <p>Action executed {counter} times.</p>
-      </div>
-    )
+  args: {
+    name: 'longAction',
+    label: 'Just do it',
+    pendingLabel: 'Doing it',
+    action: fn(({ warn }) => warn(WARNING)),
   },
   play: async ({ canvasElement }) => {
+    // We get a button
     const canvas = within(canvasElement)
-    const button = canvas.getAllByRole('button', { name: 'Test button' })[0]
+    const button = canvas.getByRole('button')
     await expect(button).toBeInTheDocument()
+
+    // Clicking will execute the action
+    await userEvent.click(button)
+
+    // Warning message will be visible
+    const warning = canvas.getByRole('field-warning')
+    await expect(warning).toHaveTextContent(WARNING)
   },
 }
 
-export const WithError: Story = {
-  render: function Example() {
-    const [counter, setCounter] = useState(0)
-    const action = useAction({
-      name: 'longAction',
-      label: 'Calculate the answer',
-      pendingLabel: 'Calculating',
-      action: async () => {
-        setCounter(c => c + 1)
-        await sleep(2000)
-        throw new Error("I don't know the answer.")
-      },
-    })
+const FAILURE = "I don't know the answer."
 
-    return (
-      <div className={'w-[400px]'}>
-        <ActionButton {...action} /> {/* This is our component */}
-        <p>Action executed {counter} times.</p>
-      </div>
-    )
+export const WithError: Story = {
+  args: {
+    name: 'calculateTheAnswer',
+    action: fn(async () => {
+      await sleep(500)
+      throw new Error(FAILURE)
+    }),
   },
   play: async ({ canvasElement }) => {
+    // We get a button
     const canvas = within(canvasElement)
-    const button = canvas.getAllByRole('button', { name: 'Test button' })[0]
+    const button = canvas.getByRole('button')
     await expect(button).toBeInTheDocument()
+
+    // Clicking will execute the action
+    await userEvent.click(button)
+
+    // Wait for it to finish
+    await sleep(550)
+
+    // Warning message will be visible
+    const error = canvas.getByRole('field-error')
+    await expect(error).toHaveTextContent(FAILURE)
   },
 }
 
 export const WithLogMessages: Story = {
-  render: function Example() {
-    const [counter, setCounter] = useState(0)
-    const action = useAction({
-      name: 'longAction',
-      label: 'Calculate the answer',
-      pendingLabel: 'Calculating',
-      action: async context => {
-        await sleep(2000)
-        context.log('Output from the action.')
-        await sleep(2000)
-        context.log('More output from the action.')
-        await sleep(2000)
-        setCounter(c => c + 1)
-      },
-    })
-
-    return (
-      <div className={'w-[400px]'}>
-        <ActionButton {...action} /> {/* This is our component */}
-        <p>Action executed {counter} times.</p>
-      </div>
-    )
+  args: {
+    name: 'longAction',
+    label: 'Calculate the answer',
+    action: async ({ log }) => {
+      await sleep(500)
+      log('Output from the action.')
+      await sleep(500)
+      log('More output from the action.')
+      await sleep(500)
+    },
   },
   play: async ({ canvasElement }) => {
+    // We get a button
     const canvas = within(canvasElement)
-    const button = canvas.getAllByRole('button', { name: 'Test button' })[0]
+    const button = canvas.getByRole('button')
     await expect(button).toBeInTheDocument()
+
+    // Clicking will execute the action
+    await userEvent.click(button)
+
+    await sleep(1600)
+
+    // Warning message will be visible
+    const messages = canvas.getAllByRole('field-info')
+    await expect(messages[0]).toHaveTextContent('Output from the action')
+    await expect(messages[1]).toHaveTextContent('More output from the action')
   },
 }
