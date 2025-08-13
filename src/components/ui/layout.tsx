4f1ce6bbbc3121daff594dc81b404452d1d35f12
lubej <matej.lubej@oasisprotocol.org>
2025-08-13 14:06:45 +0200

    layout: Use full window height

--- src/components/ui/layout.tsx
+++ src/components/ui/layout.tsx
@@ -67,19 +67,19 @@ export const Layout: FC<PropsWithChildren<LayoutProps>> = ({
           {headerBreadcrumbsContent && (
             <HeaderBreadcrumbs hasSidebar={!!sidebar}>{headerBreadcrumbsContent}</HeaderBreadcrumbs>
           )}
-          <main className="flex-1">{children}</main>
+          <main className="flex-1 min-h-0">{children}</main>
         </>
       )}
 
       {!isMobile && (
-        <div className="flex-1 overflow-hidden">
+        <div className="flex-1 overflow-hidden min-h-0">
           <div className="flex h-full">
             {sidebar && sidebar}
-            <div className="flex flex-col flex-1">
+            <div className="flex flex-col flex-1 min-h-0">
               {headerBreadcrumbsContent && (
                 <HeaderBreadcrumbs hasSidebar={!!sidebar}>{headerBreadcrumbsContent}</HeaderBreadcrumbs>
               )}
-              <main className="flex-1 overflow-y-auto">{children}</main>
+              <main className="flex-1 overflow-y-auto min-h-0">{children}</main>
             </div>
           </div>
         </div>
