c7ca9765dab3eff35cf086cc15fad987659e152a
Kristof Csillag <kristof@oasisprotocol.org>
2025-11-01 01:07:19 +0100

    Internal refact on input

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -52,21 +52,6 @@ function Input({
   const startStyle = !!startDecorationWidth ? { paddingLeft: `${16 + startDecorationWidth}px` } : {}
   const endStyle = !!endDecorationWidth ? { paddingRight: `${16 + endDecorationWidth}px` } : {}
 
-  const input = (
-    <input
-      type={type}
-      data-slot="input"
-      className={cn(
-        'file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input flex h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
-        'focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]',
-        'aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive',
-        className
-      )}
-      style={{ ...startStyle, ...endStyle }}
-      {...props}
-    />
-  )
-
   // const hasInsideDecorations = !!startDecoration || !!endDecoration
   // const hasOutsideDecorations = !!beforeStartDecoration || !!afterEndDecoration
   return (
@@ -78,7 +63,18 @@ function Input({
             {startDecoration}
           </span>
         )}
-        {input}
+        <input
+          type={type}
+          data-slot="input"
+          className={cn(
+            'file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input flex h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm',
+            'focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]',
+            'aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive',
+            className
+          )}
+          style={{ ...startStyle, ...endStyle }}
+          {...props}
+        />
         {!!endDecoration && (
           <span className={'absolute right-2.5 top-2.5'} ref={endDecorationRef}>
             {endDecoration}
